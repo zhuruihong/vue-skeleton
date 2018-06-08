d26c1b823ff3221d9f42370304c341e967ac40f4
zhaoxnshow <zhaoxnshow@163.com>
2018-06-08 11:40:57 +0800

    update

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -1,11 +1,11 @@
-require('babel-register')
-var config = require('../../config')
+require('babel-register');
+var config = require('../../config');
 
 // http://nightwatchjs.org/gettingstarted#settings-file
 module.exports = {
-  src_folders: ['example2/e2e/specs'],
-  output_folder: 'example2/e2e/reports',
-  custom_assertions_path: ['example2/e2e/custom-assertions'],
+  src_folders: ['test/e2e/specs'],
+  output_folder: 'test/e2e/reports',
+  custom_assertions_path: ['test/e2e/custom-assertions'],
 
   selenium: {
     start_process: true,
@@ -43,4 +43,4 @@ module.exports = {
       }
     }
   }
-}
+};
